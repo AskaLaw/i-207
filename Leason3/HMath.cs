193d736f802af0a850e718e54bc8ad314ad65cb8
25301548 <25301548@K216W24>
2026-09-09 09:56:55 +0500

    Добавьте файлы проекта.

---
+++ Leason3/HMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leason3
+{
+    internal class HMath: Class207.Math
+    {
+        int age = 5;
+        float height = 20.5f;
+        string Name;
+
+        void test1()
+        {
+            sum(3, 7);
+        }
+    }
+}
---
+++ Leson1/MMath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Leson1
+{
+    public class MMath : Class207.Math
+    {
+        void M1()
+        {
+            Console.WriteLine("I am from class Mama");
+        }
+    }
+}
